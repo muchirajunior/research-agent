4112fa9e196b2f0dc2ec4aefd6ce8f1f96e1e02b
muchirajunior <muchirajunior022@gmail.com>
2025-06-27 23:29:00 +0300

    Refactor main.py to implement research assistant functionality and update requirements.txt for necessary dependencies

--- main.py
+++ main.py
@@ -1,12 +1,48 @@
 from dotenv import load_dotenv
 from pydantic import BaseModel
-from langchain_openai import OpenAI
-
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
 
 load_dotenv()
 
-llm = OpenAI()
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
 
-response = llm.invoke("Hello there my name is john...")
+llm = ChatOpenAI()
 
-print(response)
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+prompt = ChatPromptTemplate.from_messages( [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use neccessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]).partial(format_instructions=parser.get_format_instructions())
+
+agent = create_tool_calling_agent(
+    llm=llm,
+    prompt=prompt,
+    tools=[]
+)
+
+agent_executor =  AgentExecutor(
+    agent=agent,
+    tools=[],
+    verbose=True
+)
+
+raw_response = agent_executor.invoke({'query':"Whats the capital city of Kenya..?"})
+
+print(raw_response)
